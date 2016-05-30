b51311890a88661a2e18ca13a6cb357afffd4edc
Petr Kubiznak <kubiznak.petr@elnico.cz>
2016-05-30 18:06:14 +0200

    Project created.
    1. Used default Eclipse configuration for Yocto CMake Hello World project.
    2. Renamed Debug configuration to debug-arm.
    3. Added CMakeEclipseDebug.txt (see "-C../CMakeEclipseDebug.txt" flag in Project -> Properties -> C/C++ Build -> Settings -> Tool Settings -> CMake configure -> CMake Options -> Other flags).
    4. Added .gitignore file.

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/** @mainpage ubiimgrdr - Elnico
+ *
+ * @author Petr Kubiznak <kubiznak.petr@elnico.cz>
+ * @version 1.0.0
+**/
+
+
+#include <stdio.h>
+/**
+ * Main class of project ubiimgrdr
+ *
+ * @param argc the number of arguments
+ * @param argv the arguments from the commandline
+ * @returns exit code of the application
+ */
+int main(int argc, char **argv) {
+	// print a greeting to the console
+	printf("Hello World!\n");
+
+	return 0;
+}
