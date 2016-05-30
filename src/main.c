360c3ecbd4d55d181a98b00b17cec9c832bf5528
Petr Kubiznak <kubiznak.petr@elnico.cz>
2016-05-30 18:54:34 +0200

    main.c: Basic "memory dump" functionality with fixed addresses implemented.

--- src/main.c
+++ src/main.c
@@ -6,6 +6,14 @@
 
 
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 /**
  * Main class of project ubiimgrdr
  *
@@ -14,8 +22,61 @@
  * @returns exit code of the application
  */
 int main(int argc, char **argv) {
-	// print a greeting to the console
-	printf("Hello World!\n");
+  int         fdMem;
+  uint32_t    u32AddrStart = 0x85000000;
+  uint32_t    u32AddrSize  = 0xA00000;   // 10 MB
+  uint32_t    u32AllocMemSize;
+  uint32_t    u32PageMask;
+  uint32_t    u32PageSize;
+  void      * pvMemPointer;
+  void      * pvVirtAddr;
+  char      * pc;
+  char      * pz;
 
-	return 0;
+  //----------------------------------- Initialization: Memory Mapping -------//
+
+  // open the /dev/mem device
+  fdMem = open("/dev/mem", O_RDONLY | O_SYNC);
+  if (-1 == fdMem) {
+    perror("open() failed");
+    return 1;
+  }
+
+  // calculate aligned address
+  u32PageSize = sysconf(_SC_PAGESIZE);
+  u32AllocMemSize = (((u32AddrSize / u32PageSize) + 1) * u32PageSize);
+  u32PageMask = (u32PageSize - 1);
+
+  // mmap the address
+  pvMemPointer = mmap(NULL,
+                      u32AllocMemSize,
+                      PROT_READ,
+                      MAP_SHARED,
+                      fdMem,
+                      (u32AddrStart & ~u32PageMask)
+                     );
+  if (MAP_FAILED == pvMemPointer) {
+    perror("mmap() failed");
+    close(fdMem);
+    return 2;
+  }
+  // here's the virtual address pointer
+  pvVirtAddr = (pvMemPointer + (u32AddrStart & u32PageMask));
+
+  //----------------------------------- Program Body -------------------------//
+
+  pc = (char*)pvVirtAddr;
+  pz = pc + u32AddrSize;
+
+  while (pc < pz) {
+    putchar(*(pc++));
+  };
+
+  //----------------------------------- Cleanup ------------------------------//
+
+  // unmap the pointer
+  munmap(pvMemPointer, u32AllocMemSize);
+  // close the /dev/mem device
+  close(fdMem);
+  return 0;
 }
