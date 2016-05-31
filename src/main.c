c53427e3be8b245664b352884258a527a0cecfbd
Petr Kubiznak <kubiznak.petr@elnico.cz>
2016-05-31 12:55:58 +0200

    main.c: Parse the XUBI header and output the UBI image only.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-/** @mainpage ubiimgrdr - Elnico
+/** @mainpage xubirdr - Elnico
  *
  * @author Petr Kubiznak <kubiznak.petr@elnico.cz>
  * @version 1.0.0
@@ -15,7 +15,7 @@
 #include <unistd.h>
 
 /**
- * Main class of project ubiimgrdr
+ * Main class of project xubirdr
  *
  * @param argc the number of arguments
  * @param argv the arguments from the commandline
@@ -24,21 +24,25 @@
 int main(int argc, char **argv) {
   int         fdMem;
   uint32_t    u32AddrStart = 0x85000000;
-  uint32_t    u32AddrSize  = 0xA00000;   // 10 MB
+  uint32_t    u32AddrSize  = 0x05000000;   // 80 MB
   uint32_t    u32AllocMemSize;
   uint32_t    u32PageMask;
   uint32_t    u32PageSize;
+  uint32_t    u32FileSize;
   void      * pvMemPointer;
   void      * pvVirtAddr;
   char      * pc;
   char      * pz;
+  uint8_t     bValid;
+  int         i;
+  int         ret = 0;
 
   //----------------------------------- Initialization: Memory Mapping -------//
 
   // open the /dev/mem device
   fdMem = open("/dev/mem", O_RDONLY | O_SYNC);
   if (-1 == fdMem) {
-    perror("open() failed");
+    perror("Error: open() failed");
     return 1;
   }
 
@@ -56,7 +60,7 @@ int main(int argc, char **argv) {
                       (u32AddrStart & ~u32PageMask)
                      );
   if (MAP_FAILED == pvMemPointer) {
-    perror("mmap() failed");
+    perror("Error: mmap() failed");
     close(fdMem);
     return 2;
   }
@@ -65,12 +69,44 @@ int main(int argc, char **argv) {
 
   //----------------------------------- Program Body -------------------------//
 
+  // data pointers
   pc = (char*)pvVirtAddr;
-  pz = pc + u32AddrSize;
+  pz = pc + u32AddrSize;      // maximum size trigger
 
-  while (pc < pz) {
-    putchar(*(pc++));
-  };
+  // check the file header
+  bValid  = (0 == strncmp(pc  , "XUBI", 4));
+  bValid &= (0 == strncmp(pc+8, "\x03\x02\x01\x00", 4));
+  if (!bValid) {
+    fprintf(stderr, "Error: Invalid XUBI header\n");
+    ret = 3;
+  }
+
+  // read the file size
+  if (0 == ret) {
+    u32FileSize = 0;
+    for (i=7; i>=4; --i) {
+      u32FileSize = (u32FileSize << 8) | *(pc+i);
+    }
+    fprintf(stderr, "UBI image file size: %lu\n", u32FileSize);
+  }
+
+  // update the data pointers
+  if (0 == ret) {
+    pc += 12;   // skip the header
+    if (pc+u32FileSize > pz) {
+      fprintf(stderr, "Error: Declared image size is out of allowed range.\n");
+      ret = 4;
+    } else {
+      pz = pc+u32FileSize;
+    }
+  }
+
+  // print the UBI file
+  if (0 == ret) {
+    while (pc < pz) {
+      putchar(*(pc++));
+    };
+  }
 
   //----------------------------------- Cleanup ------------------------------//
 
@@ -78,5 +114,5 @@ int main(int argc, char **argv) {
   munmap(pvMemPointer, u32AllocMemSize);
   // close the /dev/mem device
   close(fdMem);
-  return 0;
+  return ret;
 }
