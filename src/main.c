884339a3953957ed931a2b037f40983d783f3490
Petr Kubiznak <kubiznak.petr@elnico.cz>
2016-05-31 19:27:18 +0200

    main.c: Program options framework, --dry-run option implemented.

--- src/main.c
+++ src/main.c
@@ -13,6 +13,38 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <getopt.h>
+
+//******************************************************************************
+
+enum {
+  RET_SUCCESS = 0,
+  RET_HELP,
+  RET_OPEN_FAILURE,
+  RET_MMAP_FAILURE,
+  RET_INVALID_HEADER,
+  RET_INVALID_ARGUMENT,
+  RET_OUT_OF_MEMORY,
+};
+
+//******************************************************************************
+
+typedef struct t_config_struct {
+  uint8_t               bDryRun;                                                // do not output the file
+} TConfig;
+
+//******************************************************************************
+
+/** Parses program arguments. */
+int parseOptions (TConfig * poConfig, int argc, char * argv[]);
+/** Prints the program usage.
+ * @param[in]   progName    Program name, e.g. "xubirdr". First program argument
+ *                          (argv[0]) can typically be used for this purpose. */
+void printUsage (const char * sProgName);
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
 
 /**
  * Main class of project xubirdr
@@ -22,6 +54,9 @@
  * @returns exit code of the application
  */
 int main(int argc, char **argv) {
+  TConfig     oConfig = {
+    .bDryRun    = 0,
+  };
   int         fdMem;
   uint32_t    u32AddrStart = 0x85000000;
   uint32_t    u32AddrSize  = 0x05000000;   // 80 MB
@@ -35,15 +70,23 @@ int main(int argc, char **argv) {
   char      * pz;
   uint8_t     bValid;
   int         i;
-  int         ret = 0;
+  int         ret = RET_SUCCESS;
 
-  //----------------------------------- Initialization: Memory Mapping -------//
+  // Initialization: Application Arguments -------------------------------------
+
+  ret = parseOptions(&oConfig, argc, argv);
+  if (RET_HELP == ret) printUsage(argv[0]);
+  if (RET_SUCCESS != ret) {
+    return ((RET_HELP == ret) ? RET_SUCCESS : ret);
+  }
+
+  // Initialization: Memory Mapping --------------------------------------------
 
   // open the /dev/mem device
   fdMem = open("/dev/mem", O_RDONLY | O_SYNC);
   if (-1 == fdMem) {
     perror("Error: open() failed");
-    return 1;
+    return RET_OPEN_FAILURE;
   }
 
   // calculate aligned address
@@ -62,12 +105,12 @@ int main(int argc, char **argv) {
   if (MAP_FAILED == pvMemPointer) {
     perror("Error: mmap() failed");
     close(fdMem);
-    return 2;
+    return RET_MMAP_FAILURE;
   }
   // here's the virtual address pointer
   pvVirtAddr = (pvMemPointer + (u32AddrStart & u32PageMask));
 
-  //----------------------------------- Program Body -------------------------//
+  // Program Body --------------------------------------------------------------
 
   // data pointers
   pc = (char*)pvVirtAddr;
@@ -78,37 +121,40 @@ int main(int argc, char **argv) {
   bValid &= (0 == strncmp(pc+8, "\x03\x02\x01\x00", 4));
   if (!bValid) {
     fprintf(stderr, "Error: Invalid XUBI header\n");
-    ret = 3;
+    ret = RET_INVALID_HEADER;
   }
 
   // read the file size
-  if (0 == ret) {
+  if (RET_SUCCESS == ret) {
     u32FileSize = 0;
     for (i=7; i>=4; --i) {
       u32FileSize = (u32FileSize << 8) | *(pc+i);
     }
     fprintf(stderr, "UBI image file size: %lu\n", u32FileSize);
+    if (oConfig.bDryRun) fprintf(stdout, "%lu", u32FileSize);
   }
 
   // update the data pointers
-  if (0 == ret) {
+  if (RET_SUCCESS == ret) {
     pc += 12;   // skip the header
     if (pc+u32FileSize > pz) {
       fprintf(stderr, "Error: Declared image size is out of allowed range.\n");
-      ret = 4;
+      ret = RET_OUT_OF_MEMORY;
     } else {
       pz = pc+u32FileSize;
     }
   }
 
   // print the UBI file
-  if (0 == ret) {
-    while (pc < pz) {
-      putchar(*(pc++));
-    };
+  if (RET_SUCCESS == ret) {
+    if (!oConfig.bDryRun) {
+      while (pc < pz) {
+        putchar(*(pc++));
+      };
+    }
   }
 
-  //----------------------------------- Cleanup ------------------------------//
+  // Cleanup -------------------------------------------------------------------
 
   // unmap the pointer
   munmap(pvMemPointer, u32AllocMemSize);
@@ -116,3 +162,71 @@ int main(int argc, char **argv) {
   close(fdMem);
   return ret;
 }
+
+//******************************************************************************
+
+int parseOptions (TConfig * poConfig, int argc, char * argv[])
+{
+  struct option   aoArgOptions [] = {
+      { "dry-run"       , no_argument       , 0,  'n' },
+      { "help"          , no_argument       , 0,  'h' },
+      { 0               , 0                 , 0,  0   },
+  };
+  int             iOptionIdx  = 0;
+  int             iOptionChar = 0;
+  int             ret;
+  int             res = RET_SUCCESS;
+
+  if (!poConfig) return RET_INVALID_ARGUMENT;
+
+  while (iOptionChar >= 0) {
+    iOptionChar = getopt_long(argc, argv, "nh", aoArgOptions, &iOptionIdx);
+
+    switch (iOptionChar) {
+    case -1:
+      // no more options
+      break;
+
+    case 0:
+      printf("iOptionChar=%d=%c, iOptionIdx=%d\n",
+          (int)iOptionChar, (char)iOptionChar, iOptionIdx);
+      break;
+
+    case 'n':
+      poConfig->bDryRun = 1;
+      break;
+
+    case 'h':
+      if (RET_SUCCESS == res) res = RET_HELP;
+      break;
+
+    default:
+      res = RET_INVALID_ARGUMENT;
+      break;
+    }
+  }
+
+  if ((res != RET_SUCCESS) && (res != RET_HELP)) {
+    printf("Invalid argument. Use --help for usage.\n");
+  }
+
+  return res;
+}
+
+//******************************************************************************
+
+void printUsage (const char * sProgName)
+{
+  fprintf(stdout,
+          "Usage: %s [ -n ] [ -h ]\n"
+          "Read UBI image from a memory at given physical address.\n\n",
+          sProgName);
+
+  fprintf(stdout,
+          "  -n, --dry-run    Do not output the UBI image, just print its size\n"
+          "                   in bytes to stdout.\n\n");
+  fprintf(stdout,
+          "  -h, --help       Invoke this help.\n\n");
+}
+
+//******************************************************************************
